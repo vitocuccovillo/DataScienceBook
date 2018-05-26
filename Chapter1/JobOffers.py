64374b97afde0859784d430b48a74d626f0de96e
vitocuccovillo <vito.cuccovillo@gmail.com>
2018-05-26 17:17:40 +0200

    Initial

---
+++ Chapter1/JobOffers.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+from sklearn.feature_extraction.text import CountVectorizer
+
+if __name__ == '__main__':
+    texts = []
+
+    for index in range(0,100,10):
+        page = 'http://indeed.com/jobs?q=data+scientist&start='+str(index)
+        web_result = requests.get(page).text
+        soup = BeautifulSoup(web_result)
+
+        for listing in soup.findAll('span', {'class': 'summary'}):
+            texts.append(listing.text)
+
+    vect = CountVectorizer(ngram_range=(1,2),stop_words='english')
+    matrix = vect.fit_transform(texts)
+    print(len(vect.get_feature_names()))
+    freqs = [(word, matrix.getcol(idx).sum()) for word, idx in vect.vocabulary_.items()]
+    # sort from largest to smallest
+    for phrase, times in sorted(freqs, key=lambda x: -x[1])[:25]:
+        print(phrase, times)
--- Chapter1/PlotViz.py
+++ Chapter1/PlotViz.py
@@ -1,6 +1,7 @@
 import seaborn as sns
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     print("START MAIN")
@@ -9,4 +10,5 @@ if __name__ == '__main__':
     data = pd.read_csv(os.path.join(script_dir,"data//Advertising.csv"), index_col=0)
     print(data.head())
     sns.pairplot(data, x_vars=['TV','radio','newspaper'], y_vars='sales')
-    # sns.pairplot(data, x_vars=['TV', 'radio', 'newspaper'], y_vars='sales', size=4.5, aspect=0.7)
+    sns.pairplot(data, x_vars=['TV', 'radio', 'newspaper'], y_vars='sales', size=4.5, aspect=0.7)
+    plt.show()
