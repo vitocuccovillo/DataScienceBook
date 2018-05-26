502c06df5ae2fde05d401a85f69ef3a9ce17b57a
vitocuccovillo <vito.cuccovillo@gmail.com>
2018-05-26 16:52:02 +0200

    Initial

--- FirstChapter.py
+++ FirstChapter.py
@@ -1,2 +1,12 @@
+import seaborn as sns
+import pandas as pd
+import os
+
 if __name__ == '__main__':
-    print("hello")
+    print("START MAIN")
+
+    script_dir = os.path.dirname(__file__)
+    data = pd.read_csv(os.path.join(script_dir,"data//Advertising.csv"), index_col=0)
+    print(data.head())
+    sns.pairplot(data, x_vars=['TV','radio','newspaper'], y_vars='sales')
+    # sns.pairplot(data, x_vars=['TV', 'radio', 'newspaper'], y_vars='sales', size=4.5, aspect=0.7)
