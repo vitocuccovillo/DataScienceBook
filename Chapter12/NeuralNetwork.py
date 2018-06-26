eab2b3cfb4ccb7b5cf6c9b4e208cd432b1b5328b
vitocuccovillo <vito.cuccovillo@gmail.com>
2018-06-26 09:28:30 +0200

    neural networks

--- Chapter12/NeuralNetwork.py
+++ Chapter12/NeuralNetwork.py
@@ -14,9 +14,9 @@ import sys
 digits = datasets.load_digits()
 X, y = digits.data, digits.target
 
-ds = ClassificationDataSet(64,1,nb_classes = 10)
-test = ClassificationDataSet(64,1,nb_classes = 10)
-training = ClassificationDataSet(64,1,nb_classes = 10)
+ds = ClassificationDataSet(64,10,nb_classes = 10)
+test = ClassificationDataSet(64,10,nb_classes = 10)
+training = ClassificationDataSet(64,10,nb_classes = 10)
 
 for k in xrange(len(X)):
     ds.addSample(ravel(X[k]), y[k])
@@ -24,10 +24,18 @@ for k in xrange(len(X)):
 test_t, training_t = ds.splitWithProportion(0.25)
 
 for k in xrange(0, test_t.getLength()):
-    test.addSample(ravel(X[k]), y[k])
+    test.addSample(test_t.getSample(k)[0], test_t.getSample(k)[1])
 
 for k in xrange(0, training_t.getLength()):
-    training.addSample(ravel(X[k]), y[k])
+    training.addSample(training_t.getSample(k)[0], training_t.getSample(k)[1])
+
+print(training.getLength())
+print(test.getLength())
+
+print(test.indim)
+print(test.outdim)
+print(training.indim)
+print(training.outdim)
 
 fnn = buildNetwork(training.indim, 64, training.outdim, outclass=SoftmaxLayer)
 trainer = BackpropTrainer(fnn, dataset = training, momentum=0.1, learningrate=0.01, verbose=True, weightdecay=0.01)
