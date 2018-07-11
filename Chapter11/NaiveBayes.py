0f5a63a63454ebb250c89cdbb1637c9b7a48a979
vitocuccovillo <vito.cuccovillo@gmail.com>
2018-07-11 17:26:52 +0200

    finished

--- Chapter11/NaiveBayes.py
+++ Chapter11/NaiveBayes.py
@@ -6,13 +6,13 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 
 df = pd.read_table("https://raw.githubusercontent.com/sinanuozdemir/sfdat22/master/data/sms.tsv",
-                   sep='\t',header=None,names=['label','msg'])
+                   sep='\t', header=None, names=['label','msg'])
 
 df.label.value_counts().plot(kind='bar')
 plt.show()
 print(df.label.value_counts() / df.shape[0])
 
-df.msg = df.msg.apply(lambda x:x.lower())
+df.msg = df.msg.apply(lambda x: x.lower())
 
 spams = df[df.label == 'spam']
 # probabilità che ci siano queste parole, sapendo la classe ossia spam
@@ -23,12 +23,12 @@ train_simple = ['call you tonight','Call me a cab', 'please call me... PLEASE 44
 
 vect = CountVectorizer()
 train_simple_dtm = vect.fit_transform(train_simple)
-df2 = pd.DataFrame(train_simple_dtm.toarray(),columns=vect.get_feature_names())
+df2 = pd.DataFrame(train_simple_dtm.toarray(), columns=vect.get_feature_names())
 print(df2.head())
 
 test_simple = ["please don't call me"]
 test_simple_dtm = vect.transform(test_simple)
-df3 = pd.DataFrame(test_simple_dtm.toarray(),columns=vect.get_feature_names())
+df3 = pd.DataFrame(test_simple_dtm.toarray(), columns=vect.get_feature_names())
 print(df3.head())
 
 X_train,X_test,y_train,y_test = train_test_split(df.msg, df.label, random_state=1)
